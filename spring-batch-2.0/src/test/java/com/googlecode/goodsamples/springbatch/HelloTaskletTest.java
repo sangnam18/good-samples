17805a4c1f1e3eaee22eedf5fced9ca9a88aa284
minslovey <minslovey@c6c50d60-273a-11df-8847-f3d5e5ef3dde>
2010-03-09 01:57:33 +0000

    Added a example using Tasklet.

---
+++ spring-batch-2.0/src/test/java/com/googlecode/goodsamples/springbatch/HelloTaskletTest.java
@@ -0,0 +1,21 @@
+package com.googlecode.goodsamples.springbatch;
+
+import static org.mockito.Mockito.*;
+
+import org.apache.commons.logging.Log;
+import org.junit.Test;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+
+public class HelloTaskletTest {
+	Tasklet O = new HelloTasklet();
+	
+	@Test
+	public void taskletShouldSayHelloThroughLog() throws Exception {
+		Log log = mock(Log.class);
+		((HelloTasklet)O).log = log;
+		
+		O.execute(null, null);
+		
+		verify(log).info("Hello");
+	}
+}
