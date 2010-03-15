72a919ba62ff618fed36a9217cca1ac73ecf681c
minslovey <minslovey@c6c50d60-273a-11df-8847-f3d5e5ef3dde>
2010-03-15 09:46:03 +0000

    Added ClassScanner to scan Entity classes automatically and applied it to JPASessionFactory

--- hibernate-3.2/src/main/java/com/googlecode/goodsamples/hibernatejpa/JPASessionFactory.java
+++ hibernate-3.2/src/main/java/com/googlecode/goodsamples/hibernatejpa/JPASessionFactory.java
@@ -1,38 +1,42 @@
 package com.googlecode.goodsamples.hibernatejpa;
 
+import javax.persistence.Entity;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 
-class JPASessionFactory {
+import com.googlecode.goodsamples.common.ClassScanner;
+
+public class JPASessionFactory {
 	private static Log log = LogFactory.getLog(JPASessionFactory.class);
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 
-    private static SessionFactory buildSessionFactory() {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-        	AnnotationConfiguration cfg = new AnnotationConfiguration();
-        	cfg = (AnnotationConfiguration) cfg.configure();
-        	cfg.addAnnotatedClass(MessageUsingJPA.class);
-        	//cfg.addClass(JPAMessage.class);
-        	//cfg.addPackage("com/googlecode/goodsamples/hibernatejpa/");
-            return cfg.buildSessionFactory();
-        }
-        catch (Throwable ex) {
-            log.error("Initial SessionFactory creation failed.", ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-    
-    public static Session getSession() {
-    	return sessionFactory.openSession();
-    }
+	private static SessionFactory buildSessionFactory() {
+		try {
+			AnnotationConfiguration cfg = new AnnotationConfiguration();
+			cfg = (AnnotationConfiguration) cfg.configure();
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
+			for (Class<?> clasz : ClassScanner.getClassesAnnotated(
+					"com.googlecode.goodsamples", Entity.class)) {
+				cfg.addAnnotatedClass(clasz);
+			}
+			return cfg.buildSessionFactory();
+		} catch (Throwable ex) {
+			log.error("Initial SessionFactory creation failed.", ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static Session getSession() {
+		return sessionFactory.openSession();
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
 
 	public static void shutdown() {
 		sessionFactory.close();
