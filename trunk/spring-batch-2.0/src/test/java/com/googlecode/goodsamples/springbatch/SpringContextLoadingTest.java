d464d1106417221d7153bce0ff8601285619fee4
minslovey <minslovey@c6c50d60-273a-11df-8847-f3d5e5ef3dde>
2010-08-23 02:08:49 +0000

    Modified ApplicationContext file path.

--- trunk/spring-batch-2.0/src/test/java/com/googlecode/goodsamples/springbatch/SpringContextLoadingTest.java
+++ trunk/spring-batch-2.0/src/test/java/com/googlecode/goodsamples/springbatch/SpringContextLoadingTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/batchContext.xml" })
+@ContextConfiguration(locations = { "classpath:batchContext.xml" })
 public class SpringContextLoadingTest {
 	@Test
 	public void springContextShouldBeLoaded() {
