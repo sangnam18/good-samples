36335e7b2714ed72a4ab5e7fefe0296989697d8b
minslovey <minslovey@c6c50d60-273a-11df-8847-f3d5e5ef3dde>
2010-09-01 12:36:22 +0000

    Added an example to show the dependency injection based on Spring 3.0.

--- trunk/spring-3.0/src/test/java/com/googlecode/goodsamples/spring/support/Dependency.java
+++ trunk/spring-3.0/src/test/java/com/googlecode/goodsamples/spring/support/Dependency.java
@@ -3,16 +3,13 @@ package com.googlecode.goodsamples.spring.support;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dependency {
 	@Autowired
-	@Qualifier("oneDataSource")
 	private DataSource oneDataSource;
 	@Autowired
-	@Qualifier("twoDataSource")
 	private DataSource twoDataSource;
 
 	public Dependency() {
