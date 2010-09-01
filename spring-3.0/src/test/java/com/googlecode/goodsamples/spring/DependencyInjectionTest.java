7692b47e1050b39495bda38f059b30d52514c338
minslovey <minslovey@c6c50d60-273a-11df-8847-f3d5e5ef3dde>
2010-09-01 12:35:37 +0000

    Added an example to show the dependency injection based on Spring 2.5.

--- spring-3.0/src/test/java/com/googlecode/goodsamples/spring/DependencyInjectionTest.java
+++ spring-3.0/src/test/java/com/googlecode/goodsamples/spring/DependencyInjectionTest.java
@@ -4,27 +4,24 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.googlecode.goodsamples.spring.support.Dependency;
 
-@SuppressWarnings("deprecation")
-public class DependencyInjectionTest extends
-		AbstractDependencyInjectionSpringContextTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:dependencyContext.xml" })
+public class DependencyInjectionTest {
+	@Autowired
 	private Dependency dependency;
 
-	public void testDependencyShouldBeInjected() {
+	@Test
+	public void dependencyShouldBeInjected() {
 		assertThat(dependency, is(notNullValue()));
 		assertThat(dependency.getOneDataSource(), is(notNullValue()));
 		assertThat(dependency.getTwoDataSource(), is(notNullValue()));
-	}
-
-	public void setDependency(Dependency dependency) {
-		this.dependency = dependency;
-	}
-
-	@Override
-	protected String[] getConfigLocations() {
-		return new String[] { "classpath:dependencyContext.xml" };
 	}
 }
--- spring-3.0/src/test/java/com/googlecode/goodsamples/spring/support/Dependency.java
+++ spring-3.0/src/test/java/com/googlecode/goodsamples/spring/support/Dependency.java
@@ -2,8 +2,17 @@ package com.googlecode.goodsamples.spring.support;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dependency {
+	@Autowired
+	@Qualifier("oneDataSource")
 	private DataSource oneDataSource;
+	@Autowired
+	@Qualifier("twoDataSource")
 	private DataSource twoDataSource;
 
 	public Dependency() {
@@ -13,15 +22,7 @@ public class Dependency {
 		return oneDataSource;
 	}
 
-	public void setOneDataSource(DataSource oneDataSource) {
-		this.oneDataSource = oneDataSource;
-	}
-
 	public DataSource getTwoDataSource() {
 		return twoDataSource;
-	}
-
-	public void setTwoDataSource(DataSource twoDataSource) {
-		this.twoDataSource = twoDataSource;
 	}
 }
