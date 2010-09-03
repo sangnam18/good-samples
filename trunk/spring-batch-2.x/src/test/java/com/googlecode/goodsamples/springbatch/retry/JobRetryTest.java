fbfe481f895d1d30dc57132fbfe8d8f01e1e7558
minslovey <minslovey@c6c50d60-273a-11df-8847-f3d5e5ef3dde>
2010-09-03 06:08:07 +0000

    Modified an example name to clarify intention of it.

---
+++ trunk/spring-batch-2.x/src/test/java/com/googlecode/goodsamples/springbatch/retry/JobRetryTest.java
@@ -0,0 +1,28 @@
+package com.googlecode.goodsamples.springbatch.retry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.googlecode.goodsamples.springbatch.AbstractJobRepositoryInitilization;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/META-INF/spring/RetryContext.xml" })
+@TransactionConfiguration(defaultRollback = true)
+@Transactional
+public class JobRetryTest extends AbstractJobRepositoryInitilization {
+	@Autowired
+	JobLauncher jobLauncher;
+	@Autowired
+	Job job;
+
+	@Test
+	public void jobShouldBeRunOnPartitionedSlaves() throws Exception {
+	}
+}
