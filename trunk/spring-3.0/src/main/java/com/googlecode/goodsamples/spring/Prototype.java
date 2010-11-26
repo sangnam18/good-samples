05124f37fb410518b0d699db37838e316f89d34d
minslovey <minslovey@c6c50d60-273a-11df-8847-f3d5e5ef3dde>
2010-11-26 07:15:25 +0000

    Added an example to show injection based on prototype scope using Provide interface on J2EE.

---
+++ trunk/spring-3.0/src/main/java/com/googlecode/goodsamples/spring/Prototype.java
@@ -0,0 +1,10 @@
+package com.googlecode.goodsamples.spring;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class Prototype {
+}
