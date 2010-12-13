83e2e6d208879e064793139df5e4cb9e1ddb20f7
minslovey <minslovey@c6c50d60-273a-11df-8847-f3d5e5ef3dde>
2010-12-13 06:53:57 +0000

    Modified to improve readability.

--- spring-batch-2.x/src/test/java/com/googlecode/goodsamples/springbatch/basic/NameProcessorTest.java
+++ spring-batch-2.x/src/test/java/com/googlecode/goodsamples/springbatch/basic/NameProcessorTest.java
@@ -6,16 +6,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.springframework.batch.item.ItemProcessor;
 
-import com.googlecode.goodsamples.springbatch.basic.Name;
-import com.googlecode.goodsamples.springbatch.basic.NameProcessor;
-
-
 public class NameProcessorTest {
 	ItemProcessor<Name, Name> O = new NameProcessor();
 
 	@Test
 	public void nameShouldBeModified() throws Exception {
-		Name name = new Name(1, "Min");
-		assertThat(O.process(name), is(new Name(1, "Min-modified")));
+		final Integer id = 1;
+		Name name = new Name(id, "Min");
+
+		Name result = O.process(name);
+
+		assertThat(result, is(new Name(id, "Min-modified")));
 	}
 }
