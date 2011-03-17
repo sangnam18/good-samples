3db6d8304c8de9c93aeace16dcaf59f595dc8c5a
minslovey <minslovey@c6c50d60-273a-11df-8847-f3d5e5ef3dde>
2011-03-17 09:48:42 +0000

    Added the memo example to show how to code spring controller by TDD.

---
+++ spring-web-3.x/src/main/java/com/googlecode/goodsamples/springweb/memo/MemoService.java
@@ -0,0 +1,10 @@
+package com.googlecode.goodsamples.springweb.memo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemoService {
+	public Object findBy(int page) {
+		return null;
+	}
+}
