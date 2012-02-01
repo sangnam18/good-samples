33a95b9dbd469ff8edce9e896e5b49be889673f1
minslovey <minslovey@c6c50d60-273a-11df-8847-f3d5e5ef3dde>
2012-02-01 06:02:00 +0000

    Improved to able pass User to controller.

--- spring-security-3.x/src/main/java/com/googlecode/goodsamples/springsecurity/preauthentication/SecuredHomeController.java
+++ spring-security-3.x/src/main/java/com/googlecode/goodsamples/springsecurity/preauthentication/SecuredHomeController.java
@@ -1,6 +1,5 @@
 package com.googlecode.goodsamples.springsecurity.preauthentication;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,10 +8,10 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SecuredHomeController {
 	@RequestMapping(value = "/secured/home", method = {RequestMethod.GET})
-	public ModelAndView securedHome() {
+	public ModelAndView securedHome(User user) {
 		ModelAndView result = new ModelAndView();
 		result.setViewName("securedHome");
-		result.addObject("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		result.addObject("user", user);
 		return result;
 	}
 }
